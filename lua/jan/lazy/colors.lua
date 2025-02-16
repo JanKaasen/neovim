46fc3b4ed0ce8e8c792fa740d2410ede0150b63d
Jan Kaasen <jtkaasen@gmail.com>
2025-02-16 15:52:44 +0100

    rosepine

--- lua/jan/lazy/colors.lua
+++ lua/jan/lazy/colors.lua
@@ -1,17 +1,23 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
+    --"catppuccin/nvim",
+    --name = "catppuccin",
+    --priority = 1000,
+    --config = function()
+    --    require("catppuccin").setup({
+    --        color_overrides = {
+    --            mocha = {
+    --                base = "#000000",
+    --                mantle = "#000000",
+    --                crust = "#000000",
+    --            },
+    --        },
+    --    })
+    --    vim.cmd.colorscheme "catppuccin"
+    --end
+    -- lua/plugins/rose-pine.lua
+    "rose-pine/neovim",
+    name = "rose-pine",
     config = function()
-        require("catppuccin").setup({
-            color_overrides = {
-                mocha = {
-                    base = "#000000",
-                    mantle = "#000000",
-                    crust = "#000000",
-                },
-            },
-        })
-        vim.cmd.colorscheme "catppuccin"
+        vim.cmd("colorscheme rose-pine")
     end
 }
