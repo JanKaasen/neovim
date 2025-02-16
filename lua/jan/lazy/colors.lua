cf4e0ee8bd2c31ae21ae811525d594409ac77e4c
Jan Kaasen <jtkaasen@gmail.com>
2025-02-16 16:03:14 +0100

    update

--- lua/jan/lazy/colors.lua
+++ lua/jan/lazy/colors.lua
@@ -1,3 +1,12 @@
+function ColorMyPencils(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
 return {
     --"catppuccin/nvim",
     --name = "catppuccin",
@@ -19,10 +28,9 @@ return {
     name = "rose-pine",
     config = function()
         require("rose-pine").setup({
-            palette = {
-                base = "#000000"
-            },
+            disable_background = true,
         })
         vim.cmd("colorscheme rose-pine")
+        ColorMyPencils()
     end
 }
