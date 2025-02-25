17c0283fde4dce6e77a863765eaf34a03694f34e
Jan Kaasen <jtkaasen@gmail.com>
2025-02-25 10:03:35 +0100

    colors

--- lua/jan/lazy/colors.lua
+++ lua/jan/lazy/colors.lua
@@ -1,62 +1,17 @@
 return {
-    'shaunsingh/nord.nvim',
-    name = "nord",
+    --    'shaunsingh/nord.nvim',
+    --    name = "nord",
+    --    config = function()
+    --        vim.g.nord_disable_background = true
+    --        vim.cmd.colorscheme "nord"
+    --    end,
+
+    'sainnhe/sonokai',
+    lazy = false,
+    priority = 1000,
     config = function()
-        vim.g.nord_disable_background = true
-        vim.cmd.colorscheme "nord"
-    end,
-    --"catppuccin/nvim",
-    --name = "catppuccin",
-    --priority = 1000,
-    --config = function()
-    --    require("catppuccin").setup({
-    --        color_overrides = {
-    --            mocha = {
-    --                base = "#000000",
-    --                mantle = "#000000",
-    --                crust = "#000000",
-    --            },
-    --        },
-    --    })
-    --    vim.cmd.colorscheme "catppuccin"
-    --    vim.api.nvim_create_autocmd("ColorScheme", {
-    --        pattern = "catppuccin",
-    --        callback = function()
-    --            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    --            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    --        end,
-    --    })
-    --end
-
-    --"rose-pine/neovim",
-    --name = "rose-pine",
-    --config = function()
-    --    require("rose-pine").setup({
-    --        disable_background = true,
-    --        styles = { transparency = true },
-    --        highlight_groups = {
-    --            TelescopeBorder = { fg = "highlight_high", bg = "none" },
-    --            TelescopeNormal = { bg = "none" },
-    --            TelescopePromptNormal = { bg = "base" },
-    --            TelescopeResultsNormal = { fg = "subtle", bg = "none" },
-    --            TelescopeSelection = { fg = "text", bg = "none" },
-    --            TelescopeSelectionCaret = { fg = "rose", bg = "none" },
-
-    --            TelescopePromptBorder = { fg = "highlight_high", bg = "none" },
-    --            TelescopeResultsBorder = { fg = "highlight_high", bg = "none" },
-    --            TelescopePreviewBorder = { fg = "highlight_high", bg = "none" },
-    --            TelescopePromptTitle = { fg = "rose", bg = "none" },
-    --            TelescopeResultsTitle = { fg = "rose", bg = "none" },
-    --            TelescopePreviewTitle = { fg = "rose", bg = "none" },
-    --        },
-    --    })
-    --    vim.cmd("colorscheme rose-pine")
-    --    vim.api.nvim_create_autocmd("ColorScheme", {
-    --        pattern = "rose-pine",
-    --        callback = function()
-    --            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    --            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    --        end,
-    --    })
-    --end
+        vim.g.sonokai_transparent_background = 1
+        vim.g.sonokai_enable_italic = true
+        vim.cmd.colorscheme('sonokai')
+    end
 }
