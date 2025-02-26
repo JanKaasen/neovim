fe61459b2cc1b1617999bec6aeffd0b0e8d3bc2a
Jan Kaasen <jtkaasen@gmail.com>
2025-02-26 06:39:27 +0100

    c

--- lua/jan/lazy/colors.lua
+++ lua/jan/lazy/colors.lua
@@ -6,12 +6,38 @@ return {
     --        vim.cmd.colorscheme "nord"
     --    end,
 
-    "sainnhe/everforest",
-    lazy = false,
-    priority = 1000,
+    "rebelot/kanagawa.nvim",
+    priority = 10000,
+    name = "kanagawa",
     config = function()
-        vim.g.everforest_transparent_background = 1
-        vim.g.everforest_enable_italic = true
-        vim.cmd.colorscheme('everforest')
+        require('kanagawa').setup({
+            compile = false,  -- enable compiling the colorscheme
+            undercurl = true, -- enable undercurls
+            commentStyle = { italic = true },
+            functionStyle = {},
+            keywordStyle = { italic = true },
+            statementStyle = { bold = true },
+            typeStyle = {},
+            transparent = false,   -- do not set background color
+            dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+            terminalColors = true, -- define vim.g.terminal_color_{0,17}
+            colors = {             -- add/modify theme and palette colors
+                palette = {},
+                theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+            },
+            overrides = function(colors) -- add/modify highlights
+                return {}
+            end,
+            theme = "dragon",    -- Load "wave" theme when 'background' option is not set
+            background = {     -- map the value of 'background' option to a theme
+                dark = "wave", -- try "dragon" !
+                light = "lotus"
+            },
+        })
+
+        -- setup must be called before loading
+        vim.cmd("colorscheme kanagawa-dragon")
     end
+
+
 }
