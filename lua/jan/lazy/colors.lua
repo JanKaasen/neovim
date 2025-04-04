09b1a2b25dede2af1a6ec33437da2375a6101c03
Jan Kaasen <jtkaasen@gmail.com>
2025-04-04 12:48:41 +0200

    updates

--- lua/jan/lazy/colors.lua
+++ lua/jan/lazy/colors.lua
@@ -6,38 +6,50 @@ return {
     --        vim.cmd.colorscheme "nord"
     --    end,
 
-    "rebelot/kanagawa.nvim",
-    priority = 10000,
-    name = "kanagawa",
-    config = function()
-        require('kanagawa').setup({
-            compile = false,  -- enable compiling the colorscheme
-            undercurl = true, -- enable undercurls
-            commentStyle = { italic = true },
-            functionStyle = {},
-            keywordStyle = { italic = true },
-            statementStyle = { bold = true },
-            typeStyle = {},
-            transparent = false,   -- do not set background color
-            dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
-            terminalColors = true, -- define vim.g.terminal_color_{0,17}
-            colors = {             -- add/modify theme and palette colors
-                palette = {},
-                theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-            },
-            overrides = function(colors) -- add/modify highlights
-                return {}
-            end,
-            theme = "dragon",    -- Load "wave" theme when 'background' option is not set
-            background = {     -- map the value of 'background' option to a theme
-                dark = "wave", -- try "dragon" !
-                light = "lotus"
-            },
-        })
+    -- "rebelot/kanagawa.nvim",
+    -- priority = 10000,
+    -- name = "kanagawa",
+    -- config = function()
+    --     require('kanagawa').setup({
+    --         compile = false,  -- enable compiling the colorscheme
+    --         undercurl = true, -- enable undercurls
+    --         commentStyle = { italic = true },
+    --         functionStyle = {},
+    --         keywordStyle = { italic = true },
+    --         statementStyle = { bold = true },
+    --         typeStyle = {},
+    --         transparent = false,   -- do not set background color
+    --         dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+    --         terminalColors = true, -- define vim.g.terminal_color_{0,17}
+    --         colors = {             -- add/modify theme and palette colors
+    --             palette = {},
+    --             theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+    --         },
+    --         overrides = function(colors) -- add/modify highlights
+    --             return {}
+    --         end,
+    --         theme = "dragon",    -- Load "wave" theme when 'background' option is not set
+    --         background = {     -- map the value of 'background' option to a theme
+    --             dark = "wave", -- try "dragon" !
+    --             light = "lotus"
+    --         },
+    --     })
 
-        -- setup must be called before loading
-        vim.cmd("colorscheme kanagawa-dragon")
-    end
+    --     -- setup must be called before loading
+    --     vim.cmd("colorscheme kanagawa-dragon")
+    -- end
+
+    -- Lazy
+    {
+        "vague2k/vague.nvim",
+        config = function()
+            -- NOTE: you do not need to call setup if you don't want to.
+            require("vague").setup({
+                -- optional configuration here
+            })
+            vim.cmd("colorscheme vague")
+        end
+    },
 
 
 }
