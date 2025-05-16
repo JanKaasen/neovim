7ff9ffc08de3b89658043303dc630506461bbcfd
Jan Trygve Kaasen <jtkaasen@gmail.com>
2025-05-16 14:25:48 +0200

    rosepine is back baby

--- lua/jan/lazy/colors.lua
+++ lua/jan/lazy/colors.lua
@@ -1,12 +1,28 @@
 return {
-    "neanias/everforest-nvim",
-    version = false,
-    lazy = false,
-    priority = 1000,
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000, -- Make sure it loads early
     config = function()
-        require("everforest").setup({
-            transparent_background_level = 100,
-        })
-        vim.cmd([[colorscheme everforest]])
+      require("rose-pine").setup({
+        variant = "main", -- Choose main variant (options: 'main', 'moon', or 'dawn')
+        dark_variant = "main", -- Dark theme variant
+        bold_vert_split = false,
+        dim_nc_background = false,
+        disable_background = true, -- Set to true for transparent background
+        disable_float_background = true, -- Transparent floating windows
+        disable_italics = false,
+        
+        -- You can customize groups of highlights
+        highlight_groups = {
+          -- Customize specific highlight groups if needed
+          -- For example:
+          -- StatusLine = { fg = "love", bg = "love", blend = 10 },
+        },
+      })
+      
+      -- Set colorscheme
+      vim.cmd("colorscheme rose-pine")
     end,
+  },
 }
