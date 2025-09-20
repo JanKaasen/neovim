c8309f0d13195b27c7deff757d00145ca9a4c0f8
Jan Trygve Kaasen <jtkaasen@gmail.com>
2025-09-20 13:55:25 +0200

    Changes

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -8,17 +8,17 @@ local config_path
 local java_cmd
 
 if os_name == "Darwin" then
-    -- macOS specific paths
-    config_path = jdtls_path .. '/config_mac'
-    java_cmd = '/opt/homebrew/Cellar/openjdk@21/21.0.7/libexec/openjdk.jdk/Contents/Home/bin/java'
+	-- macOS specific paths
+	config_path = jdtls_path .. '/config_mac'
+	java_cmd = '/opt/homebrew/Cellar/openjdk@21/21.0.8/libexec/openjdk.jdk/Contents/Home/bin/java'
 elseif os_name == "Linux" then
-    -- Linux specific paths
-    config_path = jdtls_path .. '/config_linux'
-    java_cmd = '/usr/bin/java'  -- Adjust this if your Java is elsewhere on Linux
+	-- Linux specific paths
+	config_path = jdtls_path .. '/config_linux'
+	java_cmd = '/usr/bin/java' -- Adjust this if your Java is elsewhere on Linux
 else
-    -- Windows or other OS
-    config_path = jdtls_path .. '/config_win'
-    java_cmd = 'java'  -- Assumes java is in PATH
+	-- Windows or other OS
+	config_path = jdtls_path .. '/config_win'
+	java_cmd = 'java' -- Assumes java is in PATH
 end
 
 -- Common path setups
@@ -29,7 +29,7 @@ local path_to_jar = vim.fn.glob(plugins_path .. 'org.eclipse.equinox.launcher_*.
 local root_markers = { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' }
 local root_dir = require('jdtls.setup').find_root(root_markers)
 if root_dir == '' then
-    root_dir = vim.fn.getcwd()
+	root_dir = vim.fn.getcwd()
 end
 
 -- Project workspace
@@ -38,24 +38,30 @@ local workspace_dir = home .. '/.cache/jdtls/workspace/' .. project_name
 
 -- Setup JDTLS config
 local config = {
-    cmd = {
-        java_cmd,
-        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-        '-Dosgi.bundles.defaultStartLevel=4',
-        '-Declipse.product=org.eclipse.jdt.ls.core.product',
-        '-Dlog.protocol=true',
-        '-Dlog.level=ALL',
-        '-Xms1g',
-        '--add-modules=ALL-SYSTEM',
-        '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-        '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-        '-jar', path_to_jar,
-        '-configuration', config_path,
-        '-data', workspace_dir,
-    },
-    root_dir = root_dir,
-    -- Rest of your configuration...
+	cmd = {
+		java_cmd,
+		'-Declipse.application=org.eclipse.jdt.ls.core.id1',
+		'-Dosgi.bundles.defaultStartLevel=4',
+		'-Declipse.product=org.eclipse.jdt.ls.core.product',
+		'-Dlog.protocol=true',
+		'-Dlog.level=ALL',
+		'-Xms1g',
+		'--add-modules=ALL-SYSTEM',
+		'--add-opens', 'java.base/java.util=ALL-UNNAMED',
+		'--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+		'-jar', path_to_jar,
+		'-configuration', config_path,
+		'-data', workspace_dir,
+	},
+	root_dir = root_dir,
+	-- Rest of your configuration...
 }
+
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.expandtab = true
+
 
 -- Start JDTLS
 jdtls.start_or_attach(config)
