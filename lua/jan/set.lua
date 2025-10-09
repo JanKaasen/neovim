4448805ad119e1d8a7a3442ad4252754c94109a0
Jan Trygve Kaasen <jtkaasen@gmail.com>
2025-10-09 22:10:01 +0200

    changes to set

--- lua/jan/set.lua
+++ lua/jan/set.lua
@@ -2,14 +2,15 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = false
 vim.opt.guicursor = ""
+vim.opt.nrformats:append("alpha")
+vim.g.have_nerd_font = true
 
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 vim.opt.tabstop = 4
