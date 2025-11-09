24ca3d0fb500c6e580b8ee90f88a426cff658169
Jan Trygve Kaasen <jtkaasen@gmail.com>
2025-11-09 20:47:02 +0100

    colors

--- lua/jan/lazy/colors.lua
+++ lua/jan/lazy/colors.lua
@@ -2,12 +2,6 @@ return {
 	"rose-pine/neovim",
 	name = "rose-pine",
 	config = function()
-		local hour = tonumber(os.date("%H"))
-
-		if hour >= 6 and hour < 20 then
-			vim.cmd("colorscheme rose-pine-dawn")
-		else
-			vim.cmd("colorscheme rose-pine")
-		end
+		vim.cmd("colorscheme rose-pine")
 	end,
 }
