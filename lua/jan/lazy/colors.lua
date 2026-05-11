684e12220400811215b0b179ed7415949e83560c
Jan Trygve Kaasen <jtkaasen@gmail.com>
2026-05-11 11:55:26 +0200

    colors

--- lua/jan/lazy/colors.lua
+++ lua/jan/lazy/colors.lua
@@ -1,12 +1,7 @@
 return {
-    "sainnhe/gruvbox-material",
+    "blazkowolf/gruber-darker.nvim",
     config = function()
-        vim.g.gruvbox_material_background = "hard"
-        vim.g.gruvbox_material_foreground = "material"
-        vim.cmd("colorscheme gruvbox-material")
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-        vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+        vim.cmd("colorscheme gruber-darker")
     end,
 }
+
